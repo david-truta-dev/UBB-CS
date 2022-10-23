1b6e8f3a918aed8ca54dfc160780293c8fe13b1c
917_David_Truta <72062575+917-Truta-David@users.noreply.github.com>
2022-10-23 18:18:07 +0300

    no servus

--- sem5/FLCD/Lab/Lab2/SymbolTable/symbol_table.py
+++ sem5/FLCD/Lab/Lab2/SymbolTable/symbol_table.py
@@ -1,19 +1,19 @@
-
 class SymbolTable:
-    def __init__(self, initial_capacity=23):
+    def __init__(self, initial_capacity=16):
         self._capacity = initial_capacity
         self._size = 0
-        self._elements = ["!!!0!!!" for i in range(self._capacity)]
+        self._elements = ["[_/\_( - o -)_/\_]" for i in range(self._capacity)]
 
     def hash_function(self, element, index):
-        return (hash(element) % self._capacity + index * (hash(element) * hash(element) % self._capacity)) % self._capacity
+        return (hash(element) % self._capacity + index * (
+                hash(element) * hash(element) % self._capacity)) % self._capacity
 
     def add(self, element):
         if element in self._elements:
             return self.get(element)
         for trial in range(self._capacity):
             current_position = self.hash_function(element, trial)
-            if self._elements[current_position] == "!!!0!!!":
+            if self._elements[current_position] == "[_/\_( - o -)_/\_]":
                 self._elements[current_position] = element
                 self._size += 1
                 return current_position
@@ -28,7 +28,7 @@ class SymbolTable:
     def __str__(self):
         res = ""
         for i in range(len(self._elements)):
-            if self._elements[i] != '!!!0!!!':
+            if self._elements[i] != "[_/\_( - o -)_/\_]":
                 res += str(i) + ' : ' + str(self._elements[i]) + '\n'
             else:
                 res += str(i) + ' : -\n'
